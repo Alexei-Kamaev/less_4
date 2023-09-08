93dec58e78ff0d16c66d0a5fae69294c7a9767ac
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-08 11:50:36 +0300

    zad-1

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 'use strict';
-{
-
-}
+    {
+        const nameProduct = prompt('Наименование товара:');
+        const quantityProduct = +prompt("Количество товара:");
+        const categoryProduct = prompt('Категория товара:');
+        const countProduct = +prompt('Цена товара:');
+        const summProduct = quantityProduct * countProduct;
+        if (isNaN(quantityProduct) || isNaN(countProduct)) {
+            console.log('Вы ввели некорректные данные');
+        }
+        else {
+            console.log(`На складе ${quantityProduct} единицы товара "${nameProduct}" на сумму ${summProduct} деревянных`);
+        }
+        //isNaN(quantityProduct) || isNaN(countProduct) 
+        //? console.log('Вы ввели некорректные данные')
+        //: console.log(`На складе ${quantityProduct} единицы товара "${nameProduct}" на сумму ${summProduct} деревянных`);
+    }
